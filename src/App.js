63c8a8dcc708bb7e24343565ea1f6f38f762b960
ehdgnsl01 <gnsl505@naver.com>
2025-11-03 15:21:38 +0900

    feat : App.js input 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { readMoney } from './src/view/InputView.js';
+import { countTickets } from './src/validators/money.js';
+
 class App {
-  async run() {}
+  async run() {
+    const money = await readMoney();
+    this.money = money;
+    this.ticketCount = countTickets(money);
+  }
 }
 
 export default App;
