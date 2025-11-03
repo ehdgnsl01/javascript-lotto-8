d93f6d7cfe699186903fe4dc628275e0ef67bdbc
ehdgnsl01 <gnsl505@naver.com>
2025-11-03 20:01:51 +0900

    feat : 상수 추가(rank,prize)

--- src/Constants.js
+++ src/Constants.js
@@ -6,5 +6,23 @@ export const LOTTO = Object.freeze({
 });
 
 export const ERROR = Object.freeze({
-  MONEY_UNIT: '[ERROR] 구입 금액은 1,000원 단위의 양의 정수여야 합니다.',
+  MONEY_UNIT: "[ERROR] 구입 금액은 1,000원 단위의 양의 정수여야 합니다.",
+});
+
+export const RANK = Object.freeze({
+  FIRST: "FIRST",
+  SECOND: "SECOND",
+  THIRD: "THIRD",
+  FOURTH: "FOURTH",
+  FIFTH: "FIFTH",
+  MISS: "MISS",
+});
+
+export const PRIZE = Object.freeze({
+  [RANK.FIRST]: 2000000000,
+  [RANK.SECOND]: 30000000,
+  [RANK.THIRD]: 1500000,
+  [RANK.FOURTH]: 50000,
+  [RANK.FIFTH]: 5000,
+  [RANK.MISS]: 0,
 });
