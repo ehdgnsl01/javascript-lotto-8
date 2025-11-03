df47d30316ea3912e842d8241495fafaffecf260
ehdgnsl01 <gnsl505@naver.com>
2025-11-03 19:35:34 +0900

    fix : lottomachine 테스트 용이성 개선

--- src/service/LottoMachine.js
+++ src/service/LottoMachine.js
@@ -2,14 +2,10 @@ import { Random } from "@woowacourse/mission-utils";
 import { LOTTO } from "../constants.js";
 import Lotto from "../Lotto.js";
 
-export function generate(count) {
+export function generate(count, rng = Random) {
   const tickets = [];
   for (let i = 0; i < count; i += 1) {
-    const nums = Random.pickUniqueNumbersInRange(
-      LOTTO.MIN,
-      LOTTO.MAX,
-      LOTTO.PICK
-    );
+    const nums = rng.pickUniqueNumbersInRange(LOTTO.MIN, LOTTO.MAX, LOTTO.PICK);
     tickets.push(new Lotto(nums));
   }
   return tickets;
