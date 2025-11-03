a272030e349efa1c4287f346cef561e2c6726c26
ehdgnsl01 <gnsl505@naver.com>
2025-11-03 19:57:10 +0900

    feat : InputView 보너스 입력 추가

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,9 +1,11 @@
 import { Console } from "@woowacourse/mission-utils";
 import { parseMoney } from "../validators/money.js";
 import { parseWinningNumbers } from "../validators/winning.js";
+import { parseBonusNumber } from "../validators/bonus.js";
 
 const PROMPT_MONEY = "구입금액을 입력해 주세요.\n";
 const PROMPT_WINNING = "당첨 번호를 입력해 주세요.\n";
+const PROMPT_BONUS = "보너스 번호를 입력해 주세요.\n";
 
 export async function readMoney() {
   while (true) {
@@ -21,6 +23,17 @@ export async function readWinningNumbers() {
     const input = await Console.readLineAsync(PROMPT_WINNING);
     try {
       return parseWinningNumbers(input);
+    } catch (e) {
+      Console.print(e.message);
+    }
+  }
+}
+
+export async function readBonusNumber(winningNumbers) {
+  while (true) {
+    const input = await Console.readLineAsync(PROMPT_BONUS);
+    try {
+      return parseBonusNumber(input, winningNumbers);
     } catch (e) {
       Console.print(e.message);
     }
