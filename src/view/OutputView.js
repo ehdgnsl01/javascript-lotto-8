5485a92b95c2053b2e0acfc811b7badfeb57cee4
ehdgnsl01 <gnsl505@naver.com>
2025-11-03 20:10:18 +0900

    feat : OutputView 통계/수익률 추가

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,4 +1,5 @@
 import { Console } from "@woowacourse/mission-utils";
+import { formatPercent } from "../utils/format.js";
 
 export function printPurchaseCount(count) {
   Console.print(`${count}개를 구매했습니다.`);
@@ -9,4 +10,16 @@ export function printTickets(tickets) {
     const line = `[${ticket.getNumbersSorted().join(", ")}]`;
     Console.print(line);
   });
+}
+
+export function printStats(lines) {
+  Console.print("당첨 통계");
+  Console.print("---");
+  for (const line of lines) {
+    Console.print(line);
+  }
+}
+
+export function printYield(percentNumber) {
+  Console.print(`총 수익률은 ${formatPercent(percentNumber)}입니다.`);
 }
