93bb9581d33c1574b0c0efd20bfb68edb2dcecd2
ehdgnsl01 <gnsl505@naver.com>
2025-11-03 19:45:29 +0900

    feat : InputView 당첨번호 입력 추가

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,13 +1,26 @@
-import { Console } from '@woowacourse/mission-utils';
-import { parseMoney } from '../validators/money.js';
+import { Console } from "@woowacourse/mission-utils";
+import { parseMoney } from "../validators/money.js";
+import { parseWinningNumbers } from "../validators/winning.js";
 
-const PROMPT_MONEY = '구입금액을 입력해 주세요.\n';
+const PROMPT_MONEY = "구입금액을 입력해 주세요.\n";
+const PROMPT_WINNING = "당첨 번호를 입력해 주세요.\n";
 
 export async function readMoney() {
   while (true) {
     const input = await Console.readLineAsync(PROMPT_MONEY);
     try {
       return parseMoney(input);
+    } catch (e) {
+      Console.print(e.message);
+    }
+  }
+}
+
+export async function readWinningNumbers() {
+  while (true) {
+    const input = await Console.readLineAsync(PROMPT_WINNING);
+    try {
+      return parseWinningNumbers(input);
     } catch (e) {
       Console.print(e.message);
     }
