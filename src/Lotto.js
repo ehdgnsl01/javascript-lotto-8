1ade1b38decfdecbaf8354949c6521e8f1512efb
ehdgnsl01 <gnsl505@naver.com>
2025-11-03 19:25:34 +0900

    feat : lotto 클래스 보강(검증,정렬 추가)

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+import { LOTTO } from "./constants.js";
+
 class Lotto {
   #numbers;
 
@@ -10,9 +12,19 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    if (new Set(numbers).size !== 6) {
+      throw new Error("[ERROR] 번호는 중복될 수 없습니다.");
+    }
+    for (const n of numbers) {
+      if (!Number.isInteger(n) || n < LOTTO.MIN || n > LOTTO.MAX) {
+        throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+      }
+    }
   }
 
-  // TODO: 추가 기능 구현
+  getNumbersSorted() {
+    return [...this.#numbers].sort((a, b) => a - b);
+  }
 }
 
 export default Lotto;
