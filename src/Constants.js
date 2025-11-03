d73a172c313fcd5651964f8a4c475d6ffb682d87
ehdgnsl01 <gnsl505@naver.com>
2025-11-03 15:17:54 +0900

    feat : constants 추가 (price, min, max, pick, error 문구)

---
+++ src/Constants.js
@@ -0,0 +1,10 @@
+export const LOTTO = Object.freeze({
+  PRICE: 1000,
+  MIN: 1,
+  MAX: 45,
+  PICK: 6,
+});
+
+export const ERROR = Object.freeze({
+  MONEY_UNIT: '[ERROR] 구입 금액은 1,000원 단위의 양의 정수여야 합니다.',
+});
